be9ec32bd190e6bc5d701bad82adb513609dbbf7
Youhao Gong 宫酉昊 <youhao.gong.o@nio.com>
2018-04-18 14:11:08 +0800

    0.0.1

---
+++ GGPaykit/GGPaykit.h
@@ -0,0 +1,19 @@
+//
+//  GGPaykit.h
+//  GGPaykit
+//
+//  Created by Youhao Gong 宫酉昊 on 2018/4/18.
+//  Copyright © 2018年 Youhao Gong 宫酉昊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GGPaykit.
+FOUNDATION_EXPORT double GGPaykitVersionNumber;
+
+//! Project version string for GGPaykit.
+FOUNDATION_EXPORT const unsigned char GGPaykitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GGPaykit/PublicHeader.h>
+
+
---
+++ GGPaykit/Sources/ARefundController.swift
@@ -0,0 +1,19 @@
+//
+//  ARefundController.swift
+//  CentralManager
+//
+//  Created by Youhao Gong 宫酉昊 on 2018/3/22.
+//  Copyright © 2018年 Youhao Gong 宫酉昊. All rights reserved.
+//
+
+import UIKit
+
+public class ARefundController: UIViewController {
+    override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
